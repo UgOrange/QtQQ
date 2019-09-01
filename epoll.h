c0830ad277e6eadbd7a7e8f83bdad226492e6f0a
Orange <v1mer@protonmail.com>
2019-09-01 15:37:27 +0800

    mysql

--- epoll.h
+++ epoll.h
@@ -75,7 +75,7 @@ void reg(int clientFd,char message[buffSize])
     cout<<uName<<" "<<passWd<<" "<<nickName<<" "<<iconId<<" "<<signature<<" "<<secureId<<" "<<secureAnswer<<endl;
     ostringstream ostr;
     
-    ostr<<"INSERT INTO user (user_name, password, nickname, signature, head_portrait_id, password_protect_id, answer) VALUES ('"<<uName<<"',"<<passWd<<"',"<<nickName<<"',"<<iconId<<"',"<<signature<<"',"<<secureId<<"',"<<secureAnswer<<"')";
+    ostr<<"INSERT INTO user (user_name, password, nickname, signature, head_portrait_id, password_protect_id, answer) VALUES ('"<<uName<<"','"<<passWd<<"','"<<nickName<<"','"<<iconId<<"','"<<signature<<"','"<<secureId<<"','"<<secureAnswer<<"')";
     string sql=ostr.str();
     if(database.query_sql(sql))
     {
