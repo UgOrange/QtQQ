da2d989f8f93ccd84aa2726130ef34805e9ece2a
Orange <v1mer@protonmail.com>
2019-09-03 11:42:33 +0800

    fix bug

--- func.h
+++ func.h
@@ -584,8 +584,9 @@ void ServerFunc::addFriend(int clientFd,char message[buffSize])
             strcat(result1,request);
         if(iter!=userlist.end())
         {   
-            send(iter->second,&result,strlen(result),0);
-            cout<<"发送给id="<<iter->second<<" data is :"<<result1<<endl;
+            int aa=send(iter->second,&result,strlen(result),0);
+            cout<<"发送给id="<<iter->second<<" data is :"<<result1<<"return is "<<aa<<endl;
+
             strcpy(result1,"send_message_succ|发送成功！");
             bzero(result1,sizeof(result1));
         }
